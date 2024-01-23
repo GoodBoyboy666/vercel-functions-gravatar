466548e19be5b39ebb64287925f8e75396c048a8
打败大狗熊 <rakiy@qq.com>
2024-01-23 23:45:25 +0800

    测试一下

--- index.go
+++ index.go
@@ -7,5 +7,7 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Access-Control-Allow-Origin", "*") // 解决跨域问题
+	w.Header().Set("content-type", "application/plain")
 	io.WriteString(w, "欢迎使用我的头像加速！")
 }
