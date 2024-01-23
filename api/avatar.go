ac064df62d34a6f1891cb7bbce828a1f9e2242b7
打败大狗熊 <rakiy@qq.com>
2024-01-24 00:34:21 +0800

    测试一下

--- api/avatar.go
+++ api/avatar.go
@@ -4,11 +4,33 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
+const allowedDomain string = "https://blog.del.pub"
+
 func AvaterHandler(w http.ResponseWriter, r *http.Request) {
+	// 加入防盗链
+	referer := r.Header.Get("Referer")
+	if false && !strings.HasPrefix(referer, allowedDomain) {
+		http.Error(w, "403 Forbidden", http.StatusForbidden)
+		return
+	}
 	query := r.URL.Query()
-	fmt.Println(query)
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "你好，世界")
+	md5, d, s := query.Get("md5"), query.Get("d"), query.Get("s")
+	gravatar := fmt.Sprintf("https://0.gravatar.com/avatar/%s?s=%s&d=%s", md5, s, d)
+	resp, err := http.Get(gravatar)
+	if err != nil {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	defer resp.Body.Close()
+
+	// 没有G级头像
+	if resp.StatusCode == http.StatusNotFound {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+	io.Copy(w, resp.Body)
 }
